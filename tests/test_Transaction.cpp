0f4928d638ab430e1067519d789d206d9739007c
beq286 <159788198+beq286@users.noreply.github.com>
2024-05-26 15:52:11 +0300

    Update test_Transaction.cpp

--- tests/test_Transaction.cpp
+++ tests/test_Transaction.cpp
@@ -2,36 +2,31 @@
 #include <Transaction.h>
 #include <gtest/gtest.h>
 
-TEST(Transaction, Banking) {
-    const int initial_balance_Alice = 10000;
-    const int initial_balance_Bob = 2000;
-    const int transaction_fee = 50;
+TEST(Transaction, Banking){
+	const int base_A = 5000, base_B = 5000, base_fee = 100;
+	
+	Account Alice(0,base_A), Bob(1,base_B);
+	Transaction test_tran;
 
-    Account Alice(0, initial_balance_Alice), Bob(1, initial_balance_Bob);
-    Transaction test_tran;
+	ASSERT_EQ(test_tran.fee(), 1);
+	test_tran.set_fee(base_fee);
+	ASSERT_EQ(test_tran.fee(), base_fee);
 
-    ASSERT_EQ(test_tran.fee(), 1);
-    test_tran.set_fee(transaction_fee);
-    ASSERT_EQ(test_tran.fee(), transaction_fee);
+	ASSERT_THROW(test_tran.Make(Alice, Alice, 1000), std::logic_error);
+	ASSERT_THROW(test_tran.Make(Alice, Bob, -50), std::invalid_argument);
+	ASSERT_THROW(test_tran.Make(Alice, Bob, 50), std::logic_error);
+	if (test_tran.fee()*2-1 >= 100)
+		ASSERT_EQ(test_tran.Make(Alice, Bob, test_tran.fee()*2-1), false);
 
-    ASSERT_THROW(test_tran.Make(Alice, Alice, 2000), std::logic_error);
-    ASSERT_THROW(test_tran.Make(Alice, Bob, -100), std::invalid_argument);
-    ASSERT_THROW(test_tran.Make(Alice, Bob, 50), std::logic_error);
+	Alice.Lock();
+	ASSERT_THROW(test_tran.Make(Alice, Bob, 1000), std::runtime_error);
+	Alice.Unlock();
 
-    if (test_tran.fee() * 2 - 1 >= 200) {
-        ASSERT_FALSE(test_tran.Make(Alice, Bob, test_tran.fee() * 2 - 1));
-    }
+	ASSERT_EQ(test_tran.Make(Alice, Bob, 1000), true);
+	ASSERT_EQ(Bob.GetBalance(), base_B+1000);	
+	ASSERT_EQ(Alice.GetBalance(), base_A-1000-base_fee);
 
-    Alice.Lock();
-    ASSERT_THROW(test_tran.Make(Alice, Bob, 2000), std::runtime_error);
-    Alice.Unlock();
-
-    ASSERT_TRUE(test_tran.Make(Alice, Bob, 2000));
-    ASSERT_EQ(Bob.GetBalance(), initial_balance_Bob + 2000);	
-    ASSERT_EQ(Alice.GetBalance(), initial_balance_Alice - 2000 - transaction_fee);
-
-    ASSERT_FALSE(test_tran.Make(Alice, Bob, 8000));
-    ASSERT_EQ(Bob.GetBalance(), initial_balance_Bob + 2000);	
-    ASSERT_EQ(Alice.GetBalance(), initial_balance_Alice - 2000 - transaction_fee);
+	ASSERT_EQ(test_tran.Make(Alice, Bob, 3900), false);
+	ASSERT_EQ(Bob.GetBalance(), base_B+1000);	
+	ASSERT_EQ(Alice.GetBalance(), base_A-1000-base_fee);
 }
-
